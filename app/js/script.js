a084f2ea5df2df70c5d0fa22f8b50e1b6c1c2d8d
Daniel <danielnaizabekov@gmail.com>
2020-01-15 14:21:22 +0600

    fished about, contacts, footer

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,30 @@
 $(function() {
+
+	//===menu===
 	document.querySelector('.menu__btn').addEventListener('click', (event) => {
 		document.querySelector('.menu__btn').classList.toggle('menu__btn_active');
 		document.querySelector('.nav').classList.toggle('nav_active');
 	});
+	//===menu===
 
-	
 
-	if(document.documentElement.clientWidth >= 990 ) {
-		document.querySelector('.product__item:last-child').style.marginBottom = '0';
-		document.querySelector('.product__item:last-child').previousElementSibling.style.marginBottom = '0';
-		document.querySelector('.product__item:last-child').previousElementSibling.previousElementSibling.style.marginBottom = '0';
-	} else if(document.documentElement.clientWidth <= 990) {
-		document.querySelector('.product__item:last-child').style.marginBottom = '0';
+
+
+	//===changeProductItemMargin===
+	let productItem = document.querySelector('.product__item:last-child');
+
+	function changeProductItemMargin() {
+		if(document.documentElement.clientWidth >= 990 ) {
+			productItem.style.marginBottom = '0';
+			productItem.previousElementSibling.style.marginBottom = '0';
+			productItem.previousElementSibling.previousElementSibling.style.marginBottom = '0';
+		} else if(document.documentElement.clientWidth < 990) {
+			productItem.style.marginBottom = '0';
+		}
 	}
+	changeProductItemMargin()
+	window.onresize = () => {
+		changeProductItemMargin()
+	}
+	//===changeProductItemMargin===
 });
